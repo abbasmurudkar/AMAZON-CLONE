f47892c2840f66ceea8f2d00a6e6e85038255a90
abbasmurudkar <abbasmurudkar18@icloud.com>
2021-12-13 20:43:54 +0530

    Reactjs

--- src/PAGES/CHECKOUT/Checkout.js
+++ src/PAGES/CHECKOUT/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useEffect }  from 'react';
+import React from 'react';
 import styled from 'styled-components'
 import CheckoutProduct from '../../COMPONENTS/CHECKOUTPRODUCTS/CheckoutProduct';
 import Subtotal from '../../COMPONENTS/SUBTOTAL/Subtotal';
