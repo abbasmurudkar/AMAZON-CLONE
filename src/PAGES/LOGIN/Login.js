94d173966d4b1a1acb5df15a70444a2b24825333
abbasmurudkar <abbasmurudkar18@icloud.com>
2021-12-25 21:45:49 +0530

    Reactjs

--- src/PAGES/LOGIN/Login.js
+++ src/PAGES/LOGIN/Login.js
@@ -4,10 +4,24 @@ import styled from 'styled-components'
 
 function Login() {
     return (
-        <LoginPage className="Login">
+        <LoginPage>
             <Link to="/">
-             <img className="Login__logo" src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="Loading" />
+                <img className="Login__logo" src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="Loading" />
             </Link>
+            <LoginContainer>
+                <h1>SIGN IN</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text" />
+                    <h5>Password</h5>
+                    <input type="password" />
+                    <button>Sign-In</button>
+                </form>
+                <p>
+                    By continuing, you agree to Amazon's Conditions of Use and Privacy Notice, our Cookies Notice and out Interest-Based Ads Notice.
+                </p>
+                <button>Create your Amazon Account</button>
+            </LoginContainer>
         </LoginPage>
     )
 }
@@ -27,4 +41,10 @@ width: 100px;
 margin-left: auto;
 margin-right: auto;
 }
+`;
+const LoginContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content:center ;
+align-items: center;
 `;
